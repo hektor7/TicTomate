745c832513197836759f9dd93c5dc18b4474e26a
Héctor Vizcaíno <hektor7@gmail.com>
2015-09-02 20:29:44 +0200

    It has been refactored and it has been added a sound when finished the time.

--- src/main/java/com/hektor7/tictomate/TicTomate.java
+++ src/main/java/com/hektor7/tictomate/TicTomate.java
@@ -1,13 +1,21 @@
 package com.hektor7.tictomate;
 
 import com.hektor7.tictomate.ui.MainScreen;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.SimpleLongProperty;
 import javafx.stage.Stage;
+
+import java.util.concurrent.TimeUnit;
 
 public class TicTomate extends Application {
 
+    long workingSeconds = 0;
+    long restingSeconds = 0;
 
     MainScreen mainScreen;
+    private AnimationTimer animationTimer;
 
     @Override
     public void start(Stage stage) {
@@ -15,6 +23,63 @@ public class TicTomate extends Application {
         this.mainScreen = new MainScreen();
         this.mainScreen.buildUi(stage);
 
+        this.setUiBehaviour();
+
         stage.show();
+    }
+
+    private void setUiBehaviour() {
+        this.mainScreen.getStopButton().setOnAction(event -> {
+            this.animationTimer.stop();
+            this.mainScreen.enableComponentsToStop();
+        });
+        this.mainScreen.getStartButton().setOnAction(event -> {
+            this.startTimer(Double.valueOf(this.mainScreen.getSliderTaskTime().getValue()).intValue(),
+                    Double.valueOf(this.mainScreen.getSliderRestTime().getValue()).intValue());
+            this.mainScreen.disableComponentsToStart();
+        });
+    }
+
+    private void startTimer(int workingMinutes, int restingMinutes) {
+        this.workingSeconds = TimeUnit.MINUTES.toSeconds(workingMinutes);
+        this.restingSeconds = TimeUnit.MINUTES.toSeconds(restingMinutes);
+        //TODO: Disable UI controls
+
+        final LongProperty lastUpdate = new SimpleLongProperty();
+
+        final long minUpdateInterval = TimeUnit.SECONDS.toNanos(1); // nanoseconds. Set to higher number to slow output.
+
+        this.animationTimer = new AnimationTimer() {
+
+            @Override
+            public void handle(long now) {
+                if (now - lastUpdate.get() > minUpdateInterval) {
+                    updateCounter();
+                    lastUpdate.set(now);
+                }
+
+            }
+        };
+        this.animationTimer.start();
+    }
+
+    private void updateCounter(){
+        if (workingSeconds == 0){
+            this.animationTimer.stop();
+            this.mainScreen.enableComponentsToStop();
+            this.mainScreen.playBell();
+        }else {
+            this.workingSeconds--;
+            this.updateCurrentTimerLabel();
+        }
+    }
+
+    private void updateCurrentTimerLabel(){
+        long seconds = this.workingSeconds % 60;
+        long totalMinutes = this.workingSeconds / 60;
+        long minutes = totalMinutes % 60;
+        StringBuilder sb = new StringBuilder();
+        sb.append(minutes).append(":").append(seconds);
+        this.mainScreen.getTimerLabel().setText(sb.toString());
     }
 }
--- src/main/java/com/hektor7/tictomate/ui/MainScreen.java
+++ src/main/java/com/hektor7/tictomate/ui/MainScreen.java
@@ -1,241 +1,248 @@
 package com.hektor7.tictomate.ui;
 
 import com.hektor7.tictomate.TicTomate;
-import javafx.animation.AnimationTimer;
-import javafx.beans.property.LongProperty;
-import javafx.beans.property.SimpleLongProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.media.AudioClip;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Class to manage the main screen of the UI.
  * Created by hector on 2/09/15.
  */
 public class MainScreen {
-    TimerTask timerTask;
-    long workingSeconds = 0;
-    long restingSeconds = 0;
-    Timer timer = new Timer();
+
+    //--> UI Components
+    private Scene scene;
+    private GridPane grid;
+
     Label timerLabel = new Label();
+    private Slider sliderTaskTime;
+    private Slider sliderRestTime;
+    private Button startButton;
+    private Button stopButton;
+    private Label sliderRestTimeLabel;
+    private Label sliderRestTimeValue;
+    private Label sliderTaskTimeLabel;
+    private Label sliderTaskTimeValue;
+    private TextField taskName;
+    private Label taskLabel;
+    private Label headerLabel;
+    //UI Components <--
+
+    public Button getStartButton() {
+        return startButton;
+    }
+
+    public Button getStopButton() {
+        return stopButton;
+    }
+
+    public TextField getTaskName() {
+        return taskName;
+    }
+
+    public Slider getSliderRestTime() {
+        return sliderRestTime;
+    }
+
+    public Slider getSliderTaskTime() {
+        return sliderTaskTime;
+    }
+
+    public Label getTimerLabel() {
+        return this.timerLabel;
+    }
 
     public void buildUi(Stage stage) {
 
         BorderPane root = new BorderPane();
         Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
-        Scene scene = new Scene(root, visualBounds.getWidth(), visualBounds.getHeight());
+        this.scene = new Scene(root, visualBounds.getWidth(), visualBounds.getHeight());
         stage.setTitle("TicTomate v0.1");
 
-        GridPane grid = this.createMainGrid(root);
+        this.createMainGrid(root);
 
-        this.createHeader(grid);
-        this.createTaskTextField(grid);
+        this.createHeader();
+        this.createTaskTextField();
 
-        Slider sliderTaskTime = this.createWorkingTimeSlider(grid);
-        Slider sliderRestTime = this.createRestingTimeSlider(grid);
+        this.createWorkingTimeSlider();
+        this.createRestingTimeSlider();
 
-        this.setupTimerLabel(grid);
-        this.createButtons(grid, sliderTaskTime, sliderRestTime);
+        this.setupTimerLabel();
+        this.createButtons();
 
         stage.getIcons().add(new Image(TicTomate.class.getResourceAsStream("/icon.png")));
-        stage.setScene(scene);
+        stage.setScene(this.scene);
 
     }
 
-    private GridPane createMainGrid(BorderPane root) {
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(1);
-        grid.setVgap(6);
-        root.setCenter(grid);
-        return grid;
+    private void createMainGrid(BorderPane root) {
+        this.grid = new GridPane();
+        this.grid.setAlignment(Pos.CENTER);
+        this.grid.setHgap(1);
+        this.grid.setVgap(6);
+        root.setCenter(this.grid);
     }
 
-    private void createButtons(GridPane grid, Slider sliderTaskTime, Slider sliderRestTime) {
-        GridPane buttonsGrid = createButtonsGrid(grid);
+    private void createButtons() {
+        GridPane buttonsGrid = this.createButtonsGrid();
 
-        Button startButton = new Button();
-        startButton.setText("Start");
-        startButton.setAlignment(Pos.CENTER_LEFT);
-        startButton.setOnAction(event -> this.startTimer(Double.valueOf(sliderTaskTime.getValue()).intValue(),
-                Double.valueOf(sliderRestTime.getValue()).intValue()));
-        buttonsGrid.add(startButton, 0, 0);
+        this.startButton = new Button();
+        this.startButton.setText("Start");
+        this.startButton.setAlignment(Pos.CENTER_LEFT);
+        buttonsGrid.add(this.startButton, 0, 0);
 
-
-        Button stopButton = new Button();
-        stopButton.setText("Stop");
-        stopButton.setAlignment(Pos.CENTER_RIGHT);
-        stopButton.setOnAction(event -> this.timer.cancel());
-        buttonsGrid.add(stopButton, 1, 0);
+        this.stopButton = new Button();
+        this.stopButton.setText("Stop");
+        this.stopButton.setAlignment(Pos.CENTER_RIGHT);
+        buttonsGrid.add(this.stopButton, 1, 0);
     }
 
-    private GridPane createButtonsGrid(GridPane grid) {
+    private GridPane createButtonsGrid() {
         GridPane buttonsGrid = new GridPane();
         buttonsGrid.setHgap(2);
         buttonsGrid.setVgap(1);
         buttonsGrid.setAlignment(Pos.CENTER);
-        grid.add(buttonsGrid, 0, 5);
+        this.grid.add(buttonsGrid, 0, 5);
         return buttonsGrid;
     }
 
-    private void setupTimerLabel(GridPane grid) {
-        timerLabel.setText("0:00");
-        timerLabel.setAlignment(Pos.CENTER);
-        grid.add(timerLabel, 0, 4);
+    private void setupTimerLabel() {
+        this.timerLabel.setText("0:00");
+        this.timerLabel.setAlignment(Pos.CENTER);
+        this.grid.add(this.timerLabel, 0, 4);
     }
 
-    private Slider createRestingTimeSlider(GridPane grid) {
+    private void createRestingTimeSlider() {
         GridPane restTimeGrid = new GridPane();
         restTimeGrid.setVgap(3);
         restTimeGrid.setHgap(1);
         restTimeGrid.setAlignment(Pos.CENTER);
-        grid.add(restTimeGrid, 0, 3);
+        this.grid.add(restTimeGrid, 0, 3);
 
-        Slider sliderRestTime = new Slider();
-        sliderRestTime.setMin(0);
-        sliderRestTime.setMax(25);
-        sliderRestTime.setValue(5);
-        sliderRestTime.setShowTickMarks(true);
-        sliderRestTime.setMajorTickUnit(5);
-        sliderRestTime.setMinorTickCount(5);
-        sliderRestTime.setBlockIncrement(1);
+        this.sliderRestTime = new Slider();
+        this.sliderRestTime.setMin(0);
+        this.sliderRestTime.setMax(25);
+        this.sliderRestTime.setValue(5);
+        this.sliderRestTime.setShowTickMarks(true);
+        this.sliderRestTime.setMajorTickUnit(5);
+        this.sliderRestTime.setMinorTickCount(5);
+        this.sliderRestTime.setBlockIncrement(1);
 
-        Label sliderRestTimeLabel = new Label("Rest time: ");
-        sliderRestTimeLabel.setLabelFor(sliderRestTime);
-        restTimeGrid.add(sliderRestTimeLabel, 0, 0);
+        this.sliderRestTimeLabel = new Label("Rest time: ");
+        this.sliderRestTimeLabel.setLabelFor(this.sliderRestTime);
+        restTimeGrid.add(this.sliderRestTimeLabel, 0, 0);
 
-        Label sliderRestTimeValue = new Label();
-        sliderRestTimeValue.setText(String.format("%.0f",sliderRestTime.getValue()));
-        sliderRestTimeValue.setLabelFor(sliderRestTime);
-        restTimeGrid.add(sliderRestTimeValue, 2, 0);
+        this.sliderRestTimeValue = new Label();
+        this.sliderRestTimeValue.setText(String.format("%.0f", this.sliderRestTime.getValue()));
+        this.sliderRestTimeValue.setLabelFor(this.sliderRestTime);
+        restTimeGrid.add(this.sliderRestTimeValue, 2, 0);
 
-        sliderRestTime.valueProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                sliderRestTimeValue.setText(String.format("%.0f", newValue));
-            }
+        this.sliderRestTime.valueProperty().addListener((observable, oldValue, newValue) -> {
+            sliderRestTimeValue.setText(String.format("%.0f", newValue));
         });
 
-        restTimeGrid.add(sliderRestTime, 1, 0);
-        return sliderRestTime;
+        restTimeGrid.add(this.sliderRestTime, 1, 0);
     }
 
-    private Slider createWorkingTimeSlider(GridPane grid) {
+    private void createWorkingTimeSlider() {
         GridPane taskTimeGrid = new GridPane();
         taskTimeGrid.setVgap(2);
         taskTimeGrid.setHgap(1);
         taskTimeGrid.setAlignment(Pos.CENTER);
-        grid.add(taskTimeGrid, 0, 2);
+        this.grid.add(taskTimeGrid, 0, 2);
 
-        Slider sliderTaskTime = new Slider();
-        sliderTaskTime.setMin(0);
-        sliderTaskTime.setMax(90);
-        sliderTaskTime.setValue(25);
-        sliderTaskTime.setShowTickMarks(true);
-        sliderTaskTime.setMajorTickUnit(5);
-        sliderTaskTime.setMinorTickCount(5);
-        sliderTaskTime.setBlockIncrement(1);
-        sliderTaskTime.setValueChanging(true);
+        this.sliderTaskTime = new Slider();
+        this.sliderTaskTime.setMin(0);
+        this.sliderTaskTime.setMax(90);
+        this.sliderTaskTime.setValue(25);
+        this.sliderTaskTime.setShowTickMarks(true);
+        this.sliderTaskTime.setMajorTickUnit(5);
+        this.sliderTaskTime.setMinorTickCount(5);
+        this.sliderTaskTime.setBlockIncrement(1);
+        this.sliderTaskTime.setValueChanging(true);
 
-        Label sliderTaskTimeLabel = new Label("Task time: ");
-        sliderTaskTimeLabel.setLabelFor(sliderTaskTime);
-        taskTimeGrid.add(sliderTaskTimeLabel, 0, 0);
+        this.sliderTaskTimeLabel = new Label("Task time: ");
+        this.sliderTaskTimeLabel.setLabelFor(this.sliderTaskTime);
+        taskTimeGrid.add(this.sliderTaskTimeLabel, 0, 0);
 
-        Label sliderTaskTimeValue = new Label();
-        sliderTaskTimeValue.setText(String.format("%.0f", sliderTaskTime.getValue()));
-        sliderTaskTimeValue.setLabelFor(sliderTaskTime);
-        taskTimeGrid.add(sliderTaskTimeValue, 2, 0);
+        this.sliderTaskTimeValue = new Label();
+        this.sliderTaskTimeValue.setText(String.format("%.0f", this.sliderTaskTime.getValue()));
+        this.sliderTaskTimeValue.setLabelFor(this.sliderTaskTime);
+        taskTimeGrid.add(this.sliderTaskTimeValue, 2, 0);
 
-        sliderTaskTime.valueProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                sliderTaskTimeValue.setText(String.format("%.0f", newValue));
-            }
+        this.sliderTaskTime.valueProperty().addListener((observable, oldValue, newValue) -> {
+            sliderTaskTimeValue.setText(String.format("%.0f", newValue));
         });
-        taskTimeGrid.add(sliderTaskTime, 1, 0);
-        return sliderTaskTime;
+        taskTimeGrid.add(this.sliderTaskTime, 1, 0);
     }
 
-    private void createTaskTextField(GridPane grid) {
+    private void createTaskTextField() {
         GridPane taskTextGrid = new GridPane();
         taskTextGrid.setAlignment(Pos.CENTER);
         taskTextGrid.setHgap(1);
         taskTextGrid.setVgap(2);
-        grid.add(taskTextGrid, 0, 1);
+        this.grid.add(taskTextGrid, 0, 1);
 
-        TextField taskName = new TextField();
-        taskTextGrid.add(taskName, 1, 0);
-        Label taskLabel = new Label("Task name: ");
-        taskLabel.setLabelFor(taskName);
-        taskTextGrid.add(taskLabel, 0, 0);
+        this.taskName = new TextField();
+        taskTextGrid.add(this.taskName, 1, 0);
+        this.taskLabel = new Label("Task name: ");
+        this.taskLabel.setLabelFor(this.taskName);
+        taskTextGrid.add(this.taskLabel, 0, 0);
     }
 
-    private void createHeader(GridPane grid) {
+    private void createHeader() {
         GridPane headerGrid = new GridPane();
         headerGrid.setAlignment(Pos.CENTER);
         headerGrid.setHgap(1);
         headerGrid.setVgap(1);
-        Label headerLabel = new Label("TicTomate");
-        headerLabel.setAlignment(Pos.CENTER);
-        headerGrid.add(headerLabel,0,0);
-        grid.add(headerGrid,0,0);
+        this.headerLabel = new Label("TicTomate");
+        this.headerLabel.setAlignment(Pos.CENTER);
+        headerGrid.add(this.headerLabel, 0, 0);
+        this.grid.add(headerGrid, 0, 0);
     }
 
-    private void startTimer(int workingMinutes, int restingMinutes) {
-        this.workingSeconds = TimeUnit.MINUTES.toSeconds(workingMinutes);
-        this.restingSeconds = TimeUnit.MINUTES.toSeconds(restingMinutes);
-        //TODO: Disable UI controls
-
-        final LongProperty lastUpdate = new SimpleLongProperty();
-
-        final long minUpdateInterval = TimeUnit.SECONDS.toNanos(1); // nanoseconds. Set to higher number to slow output.
-
-        AnimationTimer animationTimer = new AnimationTimer() {
-
-            @Override
-            public void handle(long now) {
-                if (now - lastUpdate.get() > minUpdateInterval) {
-                    updateCounter();
-                    lastUpdate.set(now);
-                }
-
-            }
-        };
-        animationTimer.start();
+    public void enableComponentsToStop() {
+        this.getTaskName().setDisable(false);
+        this.getSliderTaskTime().setDisable(false);
+        this.getSliderRestTime().setDisable(false);
+        this.getStartButton().setDisable(false);
     }
 
-    private void updateCounter(){
-        if (workingSeconds == 0){
-            timer.cancel();
-            //TODO: Enable UI controls
-        }else {
-            this.workingSeconds--;
-            this.updateCurrentTimerLabel();
+    public void disableComponentsToStart() {
+        this.getTaskName().setDisable(true);
+        this.getSliderTaskTime().setDisable(true);
+        this.getSliderRestTime().setDisable(true);
+        this.getStartButton().setDisable(true);
+    }
+
+    public void playBell() {
+        URI uri = null;
+        try {
+            uri = this.getClass().getResource("/bell.wav").toURI();
+        } catch (URISyntaxException e) {
+            this.showError("Ooops, I can't play the bell's sound!");
         }
+        AudioClip audioClip = new AudioClip(uri.toString());
+        audioClip.play();
     }
 
-    private void updateCurrentTimerLabel(){
-        long seconds = this.workingSeconds % 60;
-        long totalMinutes = this.workingSeconds / 60;
-        long minutes = totalMinutes % 60;
-        StringBuffer sb = new StringBuffer();
-        sb.append(minutes).append(":").append(seconds);
-        this.timerLabel.setText(sb.toString());
-    }
+    private void showError(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Unexpected error");
+        alert.setHeaderText("Error");
+        alert.setContentText(message);
 
+        alert.showAndWait();
+    }
 }
