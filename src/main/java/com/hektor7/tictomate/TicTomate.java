a8b42aa9ba94199021d732fe84adda6cce61ce5b
Héctor Vizcaíno <hektor7@gmail.com>
2015-10-01 18:01:40 +0200

    Main screen runs (without functionality) on PC and Android. It's necessary to move dinamically components in Android mode in order to show all components properly.

--- src/main/java/com/hektor7/tictomate/TicTomate.java
+++ src/main/java/com/hektor7/tictomate/TicTomate.java
@@ -3,8 +3,10 @@ package com.hektor7.tictomate;
 import com.hektor7.tictomate.ui.FXMLPage;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,17 +18,29 @@ public class TicTomate extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        URL url = FXMLPage.MAIN.getPageUrl();
+        this.arrangeView(stage);
 
+    }
+
+    private void arrangeView(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(FXMLPage.MAIN.getPageUrl());
 
-        Scene scene = new Scene(root);
+
+        Scene scene = this.isDesktop() ? new Scene(root) : createSceneForMobile(root);
 
         stage.setScene(scene);
         stage.setTitle("TicTomate v0.1");
         stage.setResizable(false);
         stage.show();
+    }
 
+    private boolean isDesktop() {
+        return "desktop".equals(System.getProperty("javafx.platform", "desktop"));
+    }
+
+    private Scene createSceneForMobile(Parent root) {
+        Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+        return new Scene(root, bounds.getWidth(), bounds.getHeight());
     }
 
     public static void main(String[] args) {
